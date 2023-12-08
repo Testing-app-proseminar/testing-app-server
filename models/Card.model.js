63bee31cd697472e942f303bf8cae1738188adc8
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-12-08 18:14:22 +0100

    Modify Card model to MongoDB

--- models/Card.model.js
+++ models/Card.model.js
@@ -1,3 +1,31 @@
+// const { Schema, model } = require("mongoose");
+
+// const cardSchema = new Schema(
+//   {
+//     createdBy: {
+//         type: Schema.Types.ObjectId,
+//         ref: "User"
+//     },
+//     title: {
+//         type: String,
+//         required: [true, "Title is required"]
+//     },
+//     status: {
+//         type: String,
+//         enum: ["To-do", "Doing", "Done"]
+//     },
+//     topic: String,
+//     category: String,
+//     content: String,
+//     link: String,
+//     consumeTime: String,
+
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
 const Card = model("Card", cardSchema);
 
 const database = require("../db");
