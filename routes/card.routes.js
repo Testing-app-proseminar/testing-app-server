8458fa2dc9325aea76bccc18ac18d4436f9ccb27
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-12-08 18:30:54 +0100

    Create cards route

---
+++ routes/card.routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+
+const Card = require("../models/Card.model");
+
+
+router.post("/cards", (req, res) => {
+  const newCard = {
+    createdBy: req.payload._id,
+    title: req.body.title,
+    status: req.body.status,
+    topic: req.body.topic,
+    category: req.body.category,
+    content: req.body.content,
+    link: req.body.link,
+    consumeTime: req.body.consumeTime,
+  };
+
+  Card.create(newCard)
+    .then((response) => res.json(response))
+    .catch((err) => {
+      console.log("Error creating a new Card...", err);
+      res.status(500).json({
+        message: "We are sorry, we couldn't create your card",
+      });
+    });
+});
+
