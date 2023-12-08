2566a69f25b7cd6c04267fd3720baa640a6e12f7
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-12-08 18:14:34 +0100

    Modify User model to MongoDB

--- models/User.model.js
+++ models/User.model.js
@@ -1,17 +1,40 @@
-const database = require("../db");
+const { Schema, model } = require("mongoose");
 
-const mySqlQuery = `CREATE TABLE if not exists Users (
-  id int(11) NOT NULL AUTO_INCREMENT primary key,
-  Name varchar(150) NOT NULL,
-  Password varchar(150) NOT NULL
-  )`;
-
-const Users = database.query(mySqlQuery, (err, result) => {
-  if (err) {
-    console.error("Error creating record:", err);
-    return;
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+  },
+  {
+    timestamps: true,
   }
-  console.log(`New Table USERS added with ID: ${result.insertId},`);
-});
+);
 
-module.exports = Users;
+const User = model("User", userSchema);
+
+module.exports = User;
+
+
+// const database = require("../db");
+
+// const mySqlQuery = `CREATE TABLE if not exists Users (
+//   id int(11) NOT NULL AUTO_INCREMENT primary key,
+//   Name varchar(150) NOT NULL,
+//   Password varchar(150) NOT NULL
+//   )`;
+
+// const Users = database.query(mySqlQuery, (err, result) => {
+//   if (err) {
+//     console.error("Error creating record:", err);
+//     return;
+//   }
+//   console.log(`New Table USERS added with ID: ${result.insertId},`);
+// });
+
+// module.exports = Users;
