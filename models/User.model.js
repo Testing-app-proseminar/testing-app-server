cba754bb157bed095ad6045d4d0ed502ba755f0d
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-12-08 18:29:30 +0100

    Export module User model

--- models/User.model.js
+++ models/User.model.js
@@ -19,22 +19,3 @@ const userSchema = new Schema(
 const User = model("User", userSchema);
 
 module.exports = User;
-
-
-// const database = require("../db");
-
-// const mySqlQuery = `CREATE TABLE if not exists Users (
-//   id int(11) NOT NULL AUTO_INCREMENT primary key,
-//   Name varchar(150) NOT NULL,
-//   Password varchar(150) NOT NULL
-//   )`;
-
-// const Users = database.query(mySqlQuery, (err, result) => {
-//   if (err) {
-//     console.error("Error creating record:", err);
-//     return;
-//   }
-//   console.log(`New Table USERS added with ID: ${result.insertId},`);
-// });
-
-// module.exports = Users;
