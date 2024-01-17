7e1f351a1db9148923a36103802d6ebbf2ed236b
Monica Sanchez <monicaa.sanchezv@gmail.com>
2024-01-17 16:49:04 +0100

    Solve conflict with DB source

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,38 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const mySql = require("mysql2");
-const database = require("../db");
-const Cards = require("../models/Card.model");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 
-  const newCard = [
-    23456789,
-    555,
-    "thisone",
-    "Done",
-    "ThisOtherOne",
-    "Adventure",
-    "Loooorem",
-    "https://try.com",
-    5,
-  ];
+//   const newCard = [
+//     23456789,
+//     555,
+//     "thisone",
+//     "Done",
+//     "ThisOtherOne",
+//     "Adventure",
+//     "Loooorem",
+//     "https://try.com",
+//     5,
+//   ];
 
-  function createCard(id, CreatedBy, Title, Status, Topic, Category, Content, Link, consumeTime) {
-    const sql = `INSERT INTO Cards(id, CreatedBy, Title, Status, Topic, Category, Content, Link, consumeTime)
-    Values(?)`;
-    database.query(sql, [id, CreatedBy, Title, Status, Topic, Category, Content, Link, consumeTime], (err, result) => {
-      if (err) {
-        console.error("Error creating record:", err);
-        return;
-      }
-      res.json(result)
-      console.log(`New record added with ID: ${result.insertId} into CARDS`);
-    });
-  }
+//   function createCard(id, CreatedBy, Title, Status, Topic, Category, Content, Link, consumeTime) {
+//     const sql = `INSERT INTO Cards(id, CreatedBy, Title, Status, Topic, Category, Content, Link, consumeTime)
+//     Values(?)`;
+//     database.query(sql, [id, CreatedBy, Title, Status, Topic, Category, Content, Link, consumeTime], (err, result) => {
+//       if (err) {
+//         console.error("Error creating record:", err);
+//         return;
+//       }
+//       res.json(result)
+//       console.log(`New record added with ID: ${result.insertId} into CARDS`);
+//     });
+//   }
 
-  createCard(newCard);
-});
+//   createCard(newCard);
+ });
 
 module.exports = router;
